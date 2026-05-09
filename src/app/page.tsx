7376c82be3bc46e8639f3ac56b5802163fec0266
palakbanik <polokbanik7@gmail.com>
2026-05-09 22:38:08 +0600

    header design and responsive done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Container from "@/components/shared/Container";
 
 export default function Home() {
     return (
-        <section>
+        <section className="min-h-[800vh]">
             <Container>
                 <h1>Home</h1>
             </Container>
